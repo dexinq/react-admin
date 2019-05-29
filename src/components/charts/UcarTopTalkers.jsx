96a2475a9375580840c48b4f4740569621ae7f63
dx <dexin_work@126.com>
2019-05-29 15:03:28 +0800

    add top talker

--- src/components/charts/UcarTopTalkers.jsx
+++ src/components/charts/UcarTopTalkers.jsx
@@ -7,90 +7,17 @@ class UcarTopTalkers extends Component {
     constructor(props) {
         super(props);
 
-    }
-
-    componentDidMount(){
-
-    };
-
-    getOptions(){
-
-        let option = {
+        this.option = {
             tooltip: {
                 trigger: 'item',
                 triggerOn: 'mousemove'
             },
             series: {
                 type: 'sankey',
-                layout:'none',
+                layout: 'none',
                 focusNodeAdjacency: 'allEdges',
-                data: [{
-                    name: '10.212.27.112'
-                }, {
-                    name: '10.212.27.17'
-                }, {
-                    name: '10.212.21.30'
-                }, {
-                    name: '10.212.21.29'
-                }, {
-                    name: '10.212.36.42'
-                }, {
-                    name: '10.204.31.118'
-                }, {
-                    name: '10.204.31.119'
-                }, {
-                    name: '10.212.27.110'
-                }, {
-                    name: '10.204.247.115'
-                }, {
-                    name: '10.204.247.112'
-                },{
-                    name: '10.204.247.116'
-                },
-                    {
-                        name: '10.212.13.21'
-                    }],
-                links: [{
-                    source: '10.212.27.112',
-                    target: '10.212.21.30',
-                    value: 2798677216
-                }, {
-                    source: '10.212.27.112',
-                    target: '10.212.21.29',
-                    value: 1431918351
-                }, {
-                    source: '10.212.27.112',
-                    target: '10.212.36.42',
-                    value: 1303648898
-                }, {
-                    source: '10.212.27.17',
-                    target: '10.212.13.21',
-                    value: 296848764
-                }, {
-                    source: '10.212.27.112',
-                    target: '10.204.31.118',
-                    value: 260379787
-                }, {
-                    source: '10.212.27.112',
-                    target: '10.212.27.110',
-                    value: 213846418
-                }, {
-                    source: '10.212.27.112',
-                    target: '10.204.31.119',
-                    value: 221858427
-                }, {
-                    source: '10.212.27.17',
-                    target: '10.204.247.115',
-                    value: 152042528
-                }, {
-                    source: '10.212.27.17',
-                    target: '10.204.247.112',
-                    value: 151942528
-                }, {
-                    source: '10.212.27.17',
-                    target: '10.204.247.116',
-                    value: 151842528
-                }],
+                data: [],
+                links: [],
                 itemStyle: {
                     normal: {
                         borderWidth: 1,
@@ -104,14 +31,27 @@ class UcarTopTalkers extends Component {
                     }
                 }
             }
-        };
-        return option;
+        }
+        this.setState({option: this.option})
     }
+
+    componentDidMount(){
+        $.get(
+            "/api/get_top_talker",
+            function (r) {
+                let res = eval(r);
+                this.option.series.data = res["data"];
+                this.option.series.links = res["links"];
+                this.setState({option: this.option})
+            }
+        ).bind(this)
+
+    };
 
     render() {
         return (
             <ReactEcharts
-                option={this.getOptions()}
+                option={this.state.option}
                 style={{height: '300px',width:'100%'}}
                 className={'react_for_echarts'}
             />
